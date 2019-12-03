38d1c8a937abceb25928b62b34a85d7f9ab85db6
Nikolas Graham <Lichtschwert@live.com>
2019-12-03 13:56:40 -0800

    Basic table manager

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 $(document).ready(start);
 
+let stopIds = [];
+
 function start() {
     setInterval(updateClock, 1000);
 }
 
 function updateClock() {
-	var date = new Date();
+    var date = new Date();
 
-	time = date.toLocaleTimeString("en-US");
-	
-	$("#clock").html(time);	
+    time = date.toLocaleTimeString("en-US");
+
+    $("#clock").html(time);
+}
+
+//WIP Could be better...
+//Needs to double check that stop exists in API before adding to list as well...
+function addStop() {
+    let addStop = true;
+        if (stopIds.includes($("#stopNumberInput").val())) {
+            addStop = false;
+        }
+
+    if (addStop == true) {
+        stopIds.push($("#stopNumberInput").val());
+        $("#TableHolder").append(
+            "<table id=tbl" + $("#stopNumberInput").val() + ">"
+            + "<tr>"
+            + "<th>" + $("#stopNumberInput").val() + "</th>"
+            + "<th>"
+            + "<Button type='button' class='btn btn-dark' className='btnRemove' onclick='removeStop(value)' value="+$("#stopNumberInput").val()+">Remove</Button>"
+            + "</th>"
+            + "</tr>"
+            + "<tr>"
+            + "<td>4</td>"
+            + "<td>Monroe-Regal</td>"
+            + "<td>2:58:00 PM (56 mins)</td>"
+            + "<td>Plaza Zone 7</td>"
+            + "</tr>"
+            + "</table>"
+        )
+    }
+}
+
+function removeStop(stopId){
+    $("#tbl" + stopId).remove();
+    if (stopIds.includes($("#stopNumberInput").val())) {
+        var index = stopIds.indexOf(stopId);
+        if (index > -1) {
+            stopIds.splice(index, 1);
+        }
+        console.log(stopIds);
+    }
 }
