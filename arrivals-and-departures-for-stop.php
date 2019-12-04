dec862e5a3eda58ce36dc9028307a32e5ad396ca
Scott Shoup <pfh.chaos@gmail.com>
2019-12-04 12:51:53 -0800

    simple php proxy for arrivals-and-departures-for-stop

---
+++ arrivals-and-departures-for-stop.php
@@ -0,0 +1,15 @@
+<?php
+include_once("creds/creds.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  if(isset($_GET["stop_id"])) {
+    $stop_id = urlencode($_GET["stop_id"]);
+    http_response_code(200);
+    echo file_get_contents("http://52.88.188.196:8080/api/api/where/arrivals-and-departures-for-stop/$stop_id.json?key=$key");
+  }
+}
+else {
+  http_response_code(400);
+  echo json_encode(array("error" => "Only GET requests are supported!"));
+}
+?>
